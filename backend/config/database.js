c7b64a096f6294809a189b65a3cf4d2959560f4d
isrlchigozie <islchigozie@gmail.com>
2025-10-01 13:14:04 +0100

    Database Changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
-const { connectionString } = require('pg/lib/defaults');
 require('dotenv').config();
+
+if (!process.env.DATABASE_URL) {
+  console.error("❌ DATABASE_URL is missing!");
+} else {
+  console.log("✅ DATABASE-URL found");
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false
   },
 });
 
